9eecd55935b2721fff8deaf2016717b8eb80e3e1
Jason Washburn <jason.washburn@gmail.com>
2025-05-25 08:05:01 -0500

    feat: Add user management with database integration and environment configuration

--- main.go
+++ main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/jasonwashburn/chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const port string = ":8080"
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -26,13 +34,28 @@ var cfg apiConfig = apiConfig{
 	fileserverHits: atomic.Int32{},
 }
 
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	cfg.dbQueries = dbQueries
+
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("GET /api/healthz", handlerHealthz)
 	mux.HandleFunc("GET /admin/metrics", handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", handlerCreateUser)
 
 	srv := http.Server{
 		Handler: mux,
@@ -60,19 +83,28 @@ func handlerMetrics(w http.ResponseWriter, _ *http.Request) {
 </html>`, cfg.fileserverHits.Load())))
 }
 
-func handlerReset(w http.ResponseWriter, _ *http.Request) {
+func handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+	if os.Getenv("PLATFORM") != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		json.NewEncoder(w).Encode(errorResponse{Error: "Forbidden"})
+		return
+	}
+	err := cfg.dbQueries.ResetUsers(r.Context())
+	if err != nil {
+		log.Printf("Error resetting users: %v\n", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errorResponse{Error: "Something went wrong"})
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0\n"))
+	log.Println("Users reset")
 }
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Body string `json:"body"`
-	}
-
-	type errorResponse struct {
-		Error string `json:"error"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -96,6 +128,45 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(struct {
 		CleanedBody string `json:"cleaned_body"`
 	}{CleanedBody: cleanedBody})
+}
+
+func handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errorResponse{Error: "Something went wrong"})
+		return
+	}
+
+	user, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		log.Printf("Error creating user: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(errorResponse{Error: "Something went wrong"})
+		return
+	}
+
+	type userResponseFormat struct {
+		ID        string    `json:"id"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+		Email     string    `json:"email"`
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(userResponseFormat{
+		ID:        user.ID.String(),
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	})
 }
 
 func replaceBadWords(body string) string {
